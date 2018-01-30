8ba3cb0b34e9b27dcf7d9be22eaec1b49a3a1242
Brittany Morris <brittmmendez@gmail.com>
2018-01-30 20:41:26 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,21 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    erb :'/figures/new'
+    #create form
+  end
+
+  post '/figures' do
+    #create it and redirect to show figures
+  end
+
+  get '/figures' do
+    @figures=Figure.all
+    erb :'/figures/index'
+    #create it and redirect to show figures
+  end
+
+
+
+
 end
