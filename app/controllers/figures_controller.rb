4f08bea2a7d92742ed0d05a5ed1f988686a96ac9
Brittany Morris <brittmmendez@gmail.com>
2018-01-30 20:52:15 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,7 +12,24 @@ class FiguresController < ApplicationController
   get '/figures' do
     @figures=Figure.all
     erb :'/figures/index'
-    #create it and redirect to show figures
+    #create index to iterate through all
+  end
+
+  get '/figures/:id' do
+    @figure=Figure.find(params[:id])
+    erb :'/figures/show'
+    #create show to display all info
+  end
+
+  get '/figures/:id/edit' do
+    @figure=Figure.find(params[:id])
+    erb :'/figures/edit'
+    #create edit form
+  end
+
+  post '/figures/:id' do
+    @figure=Figure.find(params[:id])
+    #write code here to update figure instace and redirect to '/figures/:id' to  show
   end
 
 
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+    #create form
+  end
 
+  post '/landmarks' do
+    #create it and redirect to show figures
+  end
+
+  get '/landmarks' do
+    @landmarks=Landmark.all
+    erb :'/landmarks/index'
+    #create index to iterate through all
+  end
+
+  get '/landmarks/:id' do
+    @landmark=Landmark.find(params[:id])
+    erb :'/landmarks/show'
+    #create show to display all info
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark=Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+    #create edit form
+  end
+
+  post '/landmarks/:id' do
+    @landmark=Landmark.find(params[:id])
+    #write code here to update figure instace and redirect to '/figures/:id' to  show
+  end
 end
